b31b873474231b4f3048606093692604238a9dab
Siraaj Khandkar <siraaj@khandkar.net>
2013-01-20 16:59:04 -0500

    Completed exercise 1.1.c

--- 01.1/tree.ml
+++ 01.1/tree.ml
@@ -1,11 +1,15 @@
+open Printf
+
+
 module type TREE = sig
   type key = string
   type 'a t
 
-  val empty : 'a t
+  val empty  : 'a t
   val insert : 'a t -> key * 'a -> 'a t
   val member : 'a t -> key -> bool
   val lookup : 'a t -> key -> 'a option
+  val print  : 'a t -> unit
 end
 
 
@@ -48,4 +52,44 @@ module Tree : TREE = struct
         lookup l key
       else
         lookup r key
+
+  let to_string t =
+    let margin = "   " in
+    let rec to_string indent t =
+      match t with
+      | Leaf -> ""
+      | Tree (l, k, v, r) ->
+        let left = to_string (margin ^ indent)  l in
+        let right =  to_string (margin ^ indent) r in
+        sprintf "%s.%s%s\n%s" right indent k left
+    in
+    to_string margin t
+
+  let print t =
+    print_endline (to_string t)
 end
+
+
+let main () =
+  let bar = String.make 80 '-' in
+
+  let xs = ["t"; "s"; "p"; "i"; "p"; "f"; "b"; "s"; "t"] in
+  let ys = ["a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"; "i"] in
+
+  let xt = List.fold_left (fun t x -> Tree.insert t (x, x)) Tree.empty xs in
+  let yt = List.fold_left (fun t y -> Tree.insert t (y, y)) Tree.empty ys in
+
+  List.iter print_string xs;
+  print_newline ();
+  print_endline bar;
+  Tree.print xt;
+
+  print_newline ();
+
+  List.iter print_string ys;
+  print_newline ();
+  print_endline bar;
+  Tree.print yt
+
+
+let () = main ()
