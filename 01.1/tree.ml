d0787612e5bac62663089b560c61961523771f77
Siraaj Khandkar <siraaj@khandkar.net>
2013-01-20 04:39:09 -0500

    Completed exercise 1.1.b

--- 01.1/tree.ml
+++ 01.1/tree.ml
@@ -1,34 +1,51 @@
 module type TREE = sig
   type key = string
-  type t
+  type 'a t
 
-  val empty : t
-  val insert : key -> t -> t
-  val member : key -> t -> bool
+  val empty : 'a t
+  val insert : 'a t -> key * 'a -> 'a t
+  val member : 'a t -> key -> bool
+  val lookup : 'a t -> key -> 'a option
 end
 
 
 module Tree : TREE = struct
   type key = string
 
-  type t = Leaf
-         | Tree of t * key * t
+  type 'a t = Tree of 'a t * key * 'a * 'a t
+            | Leaf
 
 
   let empty = Leaf
 
-  let rec insert key = function
-    | Leaf -> Tree (Leaf, key, Leaf)
-    | Tree (l, k, r) ->
+  let rec insert t (key, value) =
+    match t with
+    | Leaf -> Tree (Leaf, key, value, Leaf)
+    | Tree (l, k, v, r) ->
       if key < k then
-        Tree (insert key l, k, r)
+        Tree (insert l (key, v), k, v, r)
       else if key > k then
-        Tree (l, k, insert key r)
+        Tree (l, k, v, insert r (key, value))
       else
-        Tree (l, key, r)
+        Tree (l, key, value, r)
 
-  let rec member key = function
+  let rec member t key =
+    match t with
     | Leaf -> false
-    | Tree (l, k, r) when key = k -> true
-    | Tree (l, k, r) -> (member key l) || (member key r)
+    | Tree (l, k, _, r) when key = k -> true
+    | Tree (l, k, _, r) ->
+      if key < k then
+        member l key
+      else
+        member r key
+
+  let rec lookup t key =
+    match t with
+    | Leaf -> None
+    | Tree (l, k, v, r) when key = k -> Some v
+    | Tree (l, k, _, r) ->
+      if key < k then
+        lookup l key
+      else
+        lookup r key
 end
